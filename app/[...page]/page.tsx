98340deb9b80ec050098fe169c6a9b897b4a2439
Savannah <savannah.foreman@commercetools.com>
2025-05-05 20:03:03 -0500

    5

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -14,16 +14,13 @@ interface PageProps {
 export default async function Page(props: PageProps) {
   const builderModelName = "page";
 
-  const content = await builder
-    // Get the page content from Builder with the specified options
-    .get(builderModelName, {
-      userAttributes: {
-        // Use the page path specified in the URL to fetch the content
-        urlPath: "/" + (props?.params?.page?.join("/") || "")
-      },
-    })
-    // Convert the result to a promise
-    .toPromise();
+  const path = props?.params?.page?.length ? "/" + props.params.page.join("/") : "/";
+
+  const content = await builder.get(builderModelName, {
+    userAttributes: {
+      urlPath: path,
+    },
+  }).toPromise();
 
   return (
     <>
